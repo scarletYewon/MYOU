1ac751d0c974d9f20518ddb51db46d800164e580
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-11-19 14:04:50 +0900

    [feature] connect

---
+++ myou/app/src/main/java/com/android/myou/Dto/Diary.kt
@@ -0,0 +1,31 @@
+package com.android.myou.Dto
+
+data class DiaryRequestDto(
+    val title : String,
+    val context : String,
+    val username : String,
+    val visibility : Boolean
+)
+data class DiaryResponseDto(
+    val title : String,
+    val context : String,
+    val username : String,
+    val createdAt : String
+)
+
+data class DiaryListDto(
+    val id : Int,
+    val title : String,
+    val summary : String,
+    val username : String,
+    val createdAt : String
+)
+data class ResponseDtoDiaryResponseDto(
+    val status : Boolean,
+    val data : DiaryResponseDto
+)
+
+data class ResponseDtoListDiaryListDto(
+    val status : Boolean,
+    val data : DiaryListDto
+)
--- myou/app/src/main/java/com/android/myou/UI/WriteMemoActivity.kt
+++ myou/app/src/main/java/com/android/myou/UI/WriteMemoActivity.kt
@@ -1,16 +1,17 @@
-package com.android.myou
+package com.android.myou.UI
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.myou.R
 
-class MyMemoActivity : AppCompatActivity() {
+class WriteMemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_my_memo)
+        setContentView(R.layout.activity_write_memo)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
